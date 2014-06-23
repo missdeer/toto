1fe0a4cb12f113c19e723d00a0a1802e577b9932
Fan Yang <missdeer@dfordsoft.com>
2014-06-23 11:29:13 +0800

    (*)use baidu search engine

--- static_source/js/main.js
+++ static_source/js/main.js
@@ -181,7 +181,8 @@
 			var q = $(this).find('[name=q]').val();
 			if($.trim(q) !== ''){
 				var host = window.location.hostname + ":" + window.location.port;
-				var url = 'http://www.google.com/search?q=' + 'site:' + host + '/p%20' +  escape($.trim(q));
+				//var url = 'http://www.google.com/search?q=' + 'site:' + host + '%20' +  $.trim(q);
+				var url = 'http://www.baidu.com/#wd=site%3Ayii.li%20' + $.trim(q);
 				window.open(url, "_blank");
 			}
 			return false;
