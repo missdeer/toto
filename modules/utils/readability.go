2b8fdaad52842d28e207adf98d4cd36405351608
missdeer <missdeer@gmail.com>
2014-07-02 17:50:53 +0800

    (+)add function declaration

--- modules/utils/readability.go
+++ modules/utils/readability.go
@@ -1,5 +1,17 @@
 package utils
 
+func renderPythonReadability(content string) string {
+	return content
+}
+
+func renderReadability(content string) string {
+	return content
+}
+
+func renderEmbedlyExtract(content string) string {
+	return content
+}
+
 func RenderReadability(content string) string {
 	return RenderMarkdown(content)
 }
