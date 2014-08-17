49cff3a9388d1b5cdc9373c887ed42aed7691467
Fan Yang <missdeer@gmail.com>
2014-08-17 10:27:22 +0800

    (*)support youku renderer

--- modules/utils/render.go
+++ modules/utils/render.go
@@ -14,21 +14,48 @@ func Render(content string) string {
 	case "!m!":
 		beego.Info("User indicates using markdown renderer!")
 		return RenderMarkdown(content[3:])
+	case "!y!":
+		beego.Info("User indicates using youku rendereer!")
+		matched := regexp.MustCompile(`^!y!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`).MatchString(content)
+		if matched {
+			raw := content[3:]
+			u, err := url.Parse(raw)
+			if err != nil {
+				beego.Error("parsing URL failed, fallthrough using markdown renderer")
+				break
+			}
+			if u.Host != "v.youku.com" {
+				beego.Error("not from youku.com, fallthrough using markdown renderer")
+				break
+			}
+			s := fmt.Sprintf(`以下内容由系统自动提取自<a href="%s" target='_blank'>%s</a>，点击该链接可访问原文，所有权利归原文出处所有。<hr/>`, raw, u.Host)
+			ids := regexp.MustCompile(`id_([A-Za-z0-9_]+)\.html`).FindStringSubmatch(raw)
+			if len(ids) <= 1 {
+				beego.Error("can't find the correct id, fallthrough using markdonw renderer!")
+				break
+			}
+			id := ids[1]
+			return s + fmt.Sprintf(`<iframe src="http://player.youku.com/embed/%s" allowfullscreen="" frameborder="0" height="440" width="680"></iframe>`, id)
+		}
+		break
 	case "!i!":
 		beego.Info("User indicates using image renderer!")
-		raw := content[3:]
-		u, err := url.Parse(raw)
-		if err != nil {
-			beego.Error("parsing URL failed, fallthrough using markdown renderer")
-			break
+		matched := regexp.MustCompile(`^!i!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`).MatchString(content)
+		if matched {
+			raw := content[3:]
+			u, err := url.Parse(raw)
+			if err != nil {
+				beego.Error("parsing URL failed, fallthrough using markdown renderer")
+				break
+			}
+			s := fmt.Sprintf(`以下内容由系统自动提取自<a href="%s" target='_blank'>%s</a>，点击该链接可访问原文，所有权利归原文出处所有。<hr/>`, raw, u.Host)
+			return s + fmt.Sprintf(`<img src="%s">`, raw)
 		}
-		s := fmt.Sprintf(`以下内容由系统自动提取自<a href="%s" target='_blank'>%s</a>，点击该链接可访问原文，所有权利归原文出处所有。<hr/>`, raw, u.Host)
-		return s + fmt.Sprintf(`<img src="%s">`, raw)
+		break
 	case "!r!":
 		beego.Info("User indicates using readability renderer!")
-		matched, err := regexp.MatchString(`^!r!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`,
-			content)
-		if err == nil && matched {
+		matched := regexp.MustCompile(`^!r!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`).MatchString(content)
+		if matched {
 			raw := content[3:]
 			u, err := url.Parse(raw)
 			if err != nil {
@@ -38,7 +65,7 @@ func Render(content string) string {
 			s := fmt.Sprintf(`以下内容由系统自动提取自<a href="%s" target='_blank'>%s</a>，点击该链接可访问原文，所有权利归原文出处所有。<hr/>`, raw, u.Host)
 			return s + RenderReadability(raw)
 		}
-		fallthrough
+		break
 	default:
 		break
 	}
