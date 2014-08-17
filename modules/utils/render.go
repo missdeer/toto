2020a6bb4c7bdb47bea9c60ad5d268268b8ee569
Fan Yang <missdeer@gmail.com>
2014-08-17 16:57:06 +0800

    (*)add error handler

--- modules/utils/render.go
+++ modules/utils/render.go
@@ -21,7 +21,19 @@ func Render(content string) string {
 		return RenderMarkdown(content[3:])
 	case "card":
 		beego.Info("User indicates using embedly renderer!")
-		return fmt.Sprintf(`<a class="embedly-card" href="%s"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>`, content[6:])
+		matched := regexp.MustCompile(`^!card!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`).MatchString(content)
+		if matched {
+			raw := content[6:]
+			_, err := url.Parse(raw)
+			if err != nil {
+				beego.Error("parsing URL failed, fallthrough using markdown renderer")
+				break
+			}
+			//s := fmt.Sprintf(`以下内容由系统自动提取自<a href="%s" target='_blank'>%s</a>，点击该链接可访问原文，所有权利归原文出处所有。<hr/>`, raw, u.Host)
+			//return s + fmt.Sprintf(`<iframe src="https://cdn.embedly.com/widgets/card.html?url=%s" allowfullscreen="" frameborder="0" width="100%" ></iframe>`, url.QueryEscape(raw))
+			return fmt.Sprintf(`<a class="embedly-card" href="%s"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>`, content[6:])
+		}
+		break
 	case "y":
 		beego.Info("User indicates using youku rendereer!")
 		matched := regexp.MustCompile(`^!y!(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})(\:[0-9]+)?(/[^/][a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@]*)*$`).MatchString(content)
