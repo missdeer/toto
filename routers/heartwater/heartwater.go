2716862053fbded23c0e6c2d877d29d88dd5705b
Fan Yang <missdeer@dfordsoft.com>
2014-10-29 15:39:18 +0800

    (+)add score pages WIP

--- routers/heartwater/heartwater.go
+++ routers/heartwater/heartwater.go
@@ -164,6 +164,7 @@ func (this *HeartwaterRouter) fetchFootballDataSource() error {
 }
 
 func (this *HeartwaterRouter) FetchFromDataSource() {
+    this.fetchFootballDataSource()
 	timer := time.NewTicker(15 * time.Second) // update data every 15 seconds
 	for {
 		select {
